41bf99f6cbfb90cf801c2afe111f78bed6bf5b67
DML_142 <demolovfennec@gmail.com>
2026-09-20 17:23:14 +0300

    fix generated database interface

--- apps/api/internal/database/db/db.go
+++ apps/api/internal/database/db/db.go
@@ -6,11 +6,12 @@ import (
 	"context"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type DBTX interface {
-	Exec(context.Context, string, ...any) (pgx.CommandTag, error)
+	Exec(context.Context, string, ...any) (pgconn.CommandTag, error)
 	Query(context.Context, string, ...any) (pgx.Rows, error)
 	QueryRow(context.Context, string, ...any) pgx.Row
 }
